0af9ce5b45698a1e626ad0aec7163c3bc9488ea5
Tamosch94 <tamer94@hotmail.de>
2019-09-01 19:15:17 +0200

    Finished Exercise 10

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
+                <h1 v-custom-on:click="myTestFunction">Directives Exercise</h1>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -12,7 +12,27 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+		directives: {
+			'custom-on': {
+				bind(el, binding, vnode) {
+					if (binding.arg == 'click') {
+						console.log('test');
+						el.addEventListener('click', function(event) {
+							binding.value();
+						});
+
+					}
+				}
+
+			}
+		},
+		methods: {
+			myTestFunction() {
+				alert('You clicked on your custom directive!')
+			}
+		}
+	}
 </script>
 
 <style>
